2990ea0b4f32d4bfa62bc1bab2cc846f620f3295
ramanlohar <143930543+ramanlohar@users.noreply.github.com>
2023-10-26 13:27:02 +0530

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,55 @@
+const omenu = document.getElementById("omenu");
+const sidebar = document.querySelector(".sidebar");
+
+omenu.addEventListener("click", () => {
+    if (sidebar.style.display === "block" || sidebar.style.display === "") {
+        sidebar.style.display = "none";
+    } else {
+        sidebar.style.display = "block";
+    }
+    omenu.classList.toggle("fa-bars");
+    omenu.classList.toggle("fa-xmark");
+});
+
+if (sidebar.style.display === "block" || sidebar.style.display === "") {
+    sidebar.style.display = "none";
+} else {
+    sidebar.style.display = "block";
+}
+
+const bcolorinput = document.querySelector("#bci");
+const savedBackgroundColor = localStorage.getItem("background-color");
+
+if (savedBackgroundColor) {
+    const cbc = document.body; // Select the body element
+    cbc.style.backgroundColor = savedBackgroundColor;
+    bcolorinput.value = savedBackgroundColor;
+}
+
+bcolorinput.addEventListener("input", () => {
+    const newBackgroundColor = bcolorinput.value;
+    localStorage.setItem("background-color", newBackgroundColor);
+    const cbc = document.body;
+    cbc.style.backgroundColor = newBackgroundColor;
+});
+
+const hfcolorinput = document.querySelector("#hfcolor");
+const savedhfColor = localStorage.getItem("hf-color");
+
+if (savedhfColor) {
+    const chfc = document.querySelector("header"); // Select the header element
+    chfc.style.backgroundColor = savedhfColor;
+    hfcolorinput.value = savedhfColor;
+
+    const cfhc = document.querySelector("footer");
+    cfhc.style.backgroundColor = savedhfColor;
+}
+
+hfcolorinput.addEventListener("input", () => {
+    const newHFColor = hfcolorinput.value;
+    localStorage.setItem("hf-color", newHFColor);
+    const chfc = document.querySelector("header");
+    chfc.style.backgroundColor = newHFColor;
+    const cfhc = document.querySelector("footer");
+    cfhc.style.backgroundColor = newHFColor;
+});
